da6d732259d6ecef70c523dde2b04fba7c7b508b
Nicholas Patel <np4795@yahoo.com>
2020-02-10 23:04:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,11 @@
 def introduction(name)
   puts"Hi my name is #{name}."
 end
+
 introduction(Nick)
 
 def introduction_with_language(name, language)
   puts"Hi my name is #{name} and I am learning to program in #{language}."
 end
+
 introduction_with_language(Nick, Ruby)
